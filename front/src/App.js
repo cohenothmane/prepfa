5f97183547df5e2581775fe43719ffeb829cfa8b
Rayan-bdo <rayanbadaoui25@gmail.com>
2025-12-26 09:53:37 +0100

    sidebar fix

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -8,15 +9,26 @@ import Inscription from "./components/inscription/Inscription";
 import Map from "./components/map/Map";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  const handleToggle = (open) => setSidebarOpen(Boolean(open));
+  const handleSearch = (query) => {
+    // Placeholder: forward search to map component or perform lookup
+    console.log("Search query:", query);
+  };
+
   return (
     <div className={`app layout-with-sidebar ${!sidebarOpen ? "sidebar-hidden" : ""}`}>
+      <Sidebar open={sidebarOpen} onToggle={handleToggle} onSearch={handleSearch} />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/inscription" element={<Inscription />} />
-      </Routes>
+      <main className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/inscription" element={<Inscription />} />
+        </Routes>
+      </main>
     </div>
   );
 }
